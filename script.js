c62f8d046db09a5b65b7d12f42dfcffdbf41b28b
Abhijeet <abhiaundhakar@gmail.com>
2024-06-16 22:31:19 +0530

    added images and animation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var sections = document.querySelectorAll('.section');
     var navLinks = document.querySelectorAll('nav ul li a');
+    var animatedSections = document.querySelectorAll('.about, .experience, #projects .row, #projects .card, .info-icons, .haveQuestion, .social-icons, #certificates .row, #certificates .card');
 
     window.addEventListener('scroll', function () {
         var currentSection = '';
@@ -19,13 +20,35 @@ document.addEventListener('DOMContentLoaded', function () {
                 link.classList.add('active');
             }
         });
+
+        // Check visibility of animated sections
+        animatedSections.forEach(checkVisibility);
+    });
+
+    function checkVisibility(section) {
+        const rect = section.getBoundingClientRect();
+        const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+        
+        if (!(rect.bottom < 0 || rect.top - viewHeight >= 0)) {
+            section.classList.add('visible');
+        }
+    }
+
+    // Initial check for visibility on page load
+    animatedSections.forEach(checkVisibility);
+
+    let linkedInButton = document.querySelector('.aboutButton');
+    linkedInButton.addEventListener('click', function () {
+        window.open('https://www.linkedin.com/in/abhijeet-aundhakar-986393200/', '_blank');
+    });
+   
+    let linkedInButtonHome = document.querySelector('#linkedInButton');
+    linkedInButtonHome.addEventListener('click', function () {
+        window.open('https://www.linkedin.com/in/abhijeet-aundhakar-986393200/', '_blank');
+    });
+
+    let githubButtonHome = document.querySelector('#githubButton');
+    githubButtonHome.addEventListener('click', function () {
+        window.open('https://github.com/AbhijeetAundhakar', '_blank');
     });
 });
-
-function redirectToGithub() {
-    window.open('https://github.com/AbhijeetAundhakar', '_blank');
-}
-
-function redirectToLinkedIn() {
-    window.open('http://linkedin.com/in/abhijeet-aundhakar-986393200/', '_blank');
-}
